4b5ff5620596484938cbee57bbffbde952e6d5bf
RikJux <60518260+RikJux@users.noreply.github.com>
2022-05-10 19:28:50 +0200

    Sensor gets the avg pollution of 8 measurements (with overlap 50%)

--- src/main/java/Simulator/SimulatorData.java
+++ src/main/java/Simulator/SimulatorData.java
@@ -6,18 +6,47 @@ import java.util.List;
 public class SimulatorData implements Buffer{
 
     private List<Measurement> measurements = new ArrayList<Measurement>();
+    private List<Measurement> averages = new ArrayList<Measurement>();
+    private double oldAvg = 0;
+    private double newAvg = 0;
 
     @Override
     public void addMeasurement(Measurement m) {
 
         measurements.add(m);
+        int size = measurements.size();
+
+        if(size % 4 == 0){
+            oldAvg = newAvg; // shift
+            newAvg = computeMean(measurements.subList(size-4, size)); // avg of last four
+            if(size % 8 == 0){
+                averages.add(new Measurement("0", "avg", (oldAvg+newAvg)/2,
+                        measurements.get(size-1).getTimestamp()));
+                // already weighted as the size of the sample is always the same
+            }
+        }
 
     }
 
     @Override
     public List<Measurement> readAllAndClean() {
 
-        return measurements;
+        return averages;
 
     }
+
+    private double computeMean(List<Measurement> meas){
+
+        double mean = 0;
+
+        for(Measurement m: meas){
+            mean += m.getValue();
+        }
+
+        mean = mean / meas.size();
+
+        return mean;
+
+    }
+
 }
--- src/main/java/TaxiPackage/Sensor.java
+++ src/main/java/TaxiPackage/Sensor.java
@@ -1,6 +1,10 @@
 package TaxiPackage;
+import Simulator.Measurement;
 import Simulator.PM10Simulator;
 import Simulator.SimulatorData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sensor extends Thread{
 
@@ -9,11 +13,10 @@ public class Sensor extends Thread{
         PM10Simulator p = new PM10Simulator(new SimulatorData());
         p.start();
         try {
-            Thread.sleep(20000);
+            Thread.sleep(10000);
+            System.out.println(p.getBuffer().readAllAndClean());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(p.getBuffer().readAllAndClean());
-
     }
 }
